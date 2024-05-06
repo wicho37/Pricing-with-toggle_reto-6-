b5fcecf8041e4234e9efbcb33cbee9129bfefa14
wicho37 <willihernandezramirez@gmail.com>
2024-05-06 07:34:33 -0500

    se continua con el css

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import Tittle from "./components/header/Header"
 import ToggleButton from "./components/boton/Button"
 import CardList from "./components/cartList/CardList"
 import style from "./App.module.css"
-import Card from "./components/card/Card"
-
+/* import Card from "./components/card/Card"
+ */
 
 
 function App() {
